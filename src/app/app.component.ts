7daeca5cd476812b90de184bad6323339921b644
Juanan <juanantonioamillopez@gmail.com>
2024-03-12 11:11:33 +0100

    Creación, import y export de componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserComponent],
   template: `
     <h1>Yo soy un componente en línea</h1>
     <h2>en un proyecto llamado {{ title }}</h2>
     <h3>estoy en {{ city.toUpperCase() }}, capital del sur</h3>
+    <app-user></app-user>
+    <br /><br />
+
+    <!-- También podemos autocerrar componentes, como en React -->
+    <app-user />
   `,
   styles: `
   h1, h2, h3{
