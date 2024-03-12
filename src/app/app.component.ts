1f5a9389740062ff0c64f9fe5009b4da9e326650
Juanan <juanantonioamillopez@gmail.com>
2024-03-12 11:19:59 +0100

    Import y export de componentes SIN TEMPLATE STRINGS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,8 @@ import { UserComponent } from './user/user.component';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, UserComponent],
-  template: `
-    <h1>Yo soy un componente en línea</h1>
-    <h2>en un proyecto llamado {{ title }}</h2>
-    <h3>estoy en {{ city.toUpperCase() }}, capital del sur</h3>
-    <app-user></app-user>
-    <br /><br />
-
-    <!-- También podemos autocerrar componentes, como en React -->
-    <app-user />
-  `,
-  styles: `
-  h1, h2, h3{
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-    color: #e00202;
-  }
-  `,
+  templateUrl: './app.component.html',
+  styles: ``,
 })
 export class AppComponent {
   title = 'HolaAngular';
